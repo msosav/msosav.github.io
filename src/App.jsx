404617ea6d8414bc5c882e730d0945179d34de11
Miguel Sosa <85181687+msosav@users.noreply.github.com.>
2024-02-19 13:02:00 -0500

    Implement Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,13 @@
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Navbar from "./components/Navbar";
+import Projects from "./components/Projects";
+import Skills from "./components/Skills";
+
 export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div class="bg-dark">
+      <Navbar />
+    </div>
+  );
 }
