f3a4617c69ca8aba9c358be20dff8f3135963643
Miguel Sosa <85181687+msosav@users.noreply.github.com.>
2024-02-19 22:04:43 -0500

    Fix images not showing when deployed

--- src/data.js
+++ src/data.js
@@ -3,18 +3,18 @@ export const projects = [
     title: "Toysnt",
     languajes: "PHP/Laravel",
     link: "https://github.com/msosav/Toysnt",
-    image: "../public/images/Toysnt.png",
+    image: "./images/Toysnt.png",
   },
   {
     title: "HotelSuite",
     languajes: "Python/Django",
     link: "https://github.com/EAFIT-Ingenieria-Software/HotelSuite",
-    image: "../public/images/HotelSuite.png",
+    image: "./images/HotelSuite.png",
   },
   {
     title: "Lookup",
     languajes: "Python/Django",
     link: "https://github.com/msosav/Lookup",
-    image: "../public/images/Lookup.png",
+    image: "./images/Lookup.png",
   },
 ];
